cc2a54e58d37da1acb650fc2689cd7f65d9d660c
Damidu Shalinda <shalindadamidu@gmail.com>
2023-07-28 02:58:51 +0530

    added home screen for customer

--- lib/Pages/staff/home.dart
+++ lib/Pages/staff/home.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:loan_managment_app/utils/usersecurestorage.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -12,22 +11,28 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-            body: Center(
-            child: Column(
-              children: [
-                  const Text('Welcome home'),
-                  TextButton(
-                      onPressed: () async {
-                      if (kDebugMode) {
-                        print(UserSecureStorage.getAcessToken().toString());
-                      }
-                      },
-                     child: const Text('print token'))
-                ],
+    return const Scaffold(
+      body: Column(
+        children: [
+          ListTile(
+            leadingAndTrailingTextStyle: TextStyle(
+              color: Colors.black,
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
             ),
+            titleTextStyle: TextStyle(
+              color: Colors.black,
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
             ),
+            title: Text("John Smith"),
+            subtitle: Text("2023-01-01"),
+            leading: CircleAvatar(
+              child: Text("JS"),
+            ),
+          ),
+        ]
+      )
     );
-
   }
 }
